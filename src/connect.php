276393d64203b49fc7d577894364cf1b0aa09be6
gjcourtney <gavin.courtney@diffuseuk.com>
2014-05-20 09:25:15 +0100

    New PHP connector example
    A simple script that connects to the ChurchApp API and GETs a contact's details.

---
+++ src/connect.php
@@ -0,0 +1,45 @@
+<?php
+
+/*****
+*
+* Basic script to connect to the API and GET a contact's details.
+*
+*****/
+
+$url = 'https://api.churchapp.co.uk/v1/addressbook/contact/1';
+
+$x_account = '';
+$x_application = '';
+$x_auth = '';
+
+$api_headers = array(
+	'Accept: application/json',
+	'Content-Type: application/json',
+	'X-Account: '.$x_account,
+	'X-Auth: '.$x_auth,
+	'X-Application: '.$x_application,
+);
+
+$ch = curl_init($url);
+
+$curlopts = array();
+$curlopts[CURLOPT_RETURNTRANSFER] = true;
+$curlopts[CURLOPT_SSLVERSION] = 3;
+$curlopts[CURLOPT_HTTPHEADER] = $api_headers;
+curl_setopt_array($ch, $curlopts);
+
+$response = curl_exec($ch);
+$error = curl_errno($ch);
+$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+$json = json_decode($response, 1);
+
+if (in_array($http_code, array(200, 201))) {
+	// OK response
+	echo '<h1>OK: '.$http_code.'</h1>';
+	var_dump($json);
+} else {
+	// got an error response
+	echo '<h1>Error: '.$http_code.'</h1>';
+	echo '<p>ChurchApp reported the following error: '.$json['error']['message'].'</p>';
+}
